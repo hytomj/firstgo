a793d08513a317afe98c8758bd8a24bd64fd1ff5
hanowa <573015178@qq.com>
2020-11-09 01:34:12 +0800

    modify hello.go

--- hello/hello.go
+++ hello/hello.go
@@ -1,10 +1,13 @@
 package main
 
-import "fmt"
-
-func 
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
 	fmt.Println("hello golang")
+	var s = []int{11, 8, 99, 0, 2}
+	sort.Sort(sort.Reverse(sort.IntSlice(s)))
+	fmt.Println(s)
 }
-

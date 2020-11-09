d95e98d5710c020874ce1be95675f96cbee65b31
Wang Jun <wangjun@hanowa-mbp.local>
2020-11-09 15:42:46 +0800

    modify

--- hello/hello.go
+++ hello/hello.go
@@ -13,7 +13,7 @@ func showArray() {
 }
 
 func main() {
-	fmt.Println("hello golang")
+	//fmt.Println("hello golang")
 	//var s = []int{11, 8, 99, 0, 2}
 	//sort.Sort(sort.Reverse(sort.IntSlice(s)))
 	//fmt.Println(s)

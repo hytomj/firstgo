fc31ad3bc41d905706d564f2cfbf6367785d3a76
Wang Jun <wangjun@hanowa-mbp.local>
2020-11-20 10:24:29 +0800

    a

--- defer_test/test_defer.go
+++ defer_test/test_defer.go
@@ -3,20 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	s := make([]int, 1, 5)
-	fmt.Printf("%v\n", s)
 
-	x, j := 22, 1003
-	//var p *int
-	p := &x
-	fmt.Println(*p)
-	p = &j
-	fmt.Println(*p)
-	fmt.Println("start...")
-
-	for i := 0; i < 10; i++ {
-		defer fmt.Println(i)
-	}
-
-	fmt.Println("end...")
 }
---
+++ map_test/test_map.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var m map[string]int
+	m["tom"] = 100
+	m["john"] = 120
+	fmt.Println(m)
+}
